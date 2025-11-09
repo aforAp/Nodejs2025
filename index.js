b396969a96006a26bc569fb14f8dc992ddf5fef3
aforAp <satheeshsubbu1612@gmail.com>
2025-11-09 20:44:16 +0530

    basics

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+///////////////////////////////
+
+/*
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 const textInto = fs.readFileSync('./txt/hello/input.txt', 'utf-8');
 console.log(textIn, textInto);
@@ -7,9 +11,42 @@ const textOut = `this is what we know about the avocado: ${textIn}.\nCreated on 
 
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written');
+*/
 
 //Non-blocking, asynchronous way
 
+fs.readFile('./txt/start.txt','utf-8', (err, data) => {
+    if (err) return console.log("ERROR");
+   fs.readFile(`./txt/${data}.txt`,'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`,'utf-8', (err, data3) => {
+    console.log(data3);
+
+    fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+      console.log("youur file has been written");
+    })
+
+    fs.writeFile('./txt/finals.txt', `${data2}\n${data3}`, 'utf-8', err => {
+        console.log('data entered');
+    })
+})
+})
+})
+
+console.log("will read file");
+
+//////////////////////////////////////////////
+//SERVER
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+})
+
+//8000 => PORT 127.0.0.1 => ip once open 127.0.0.1
 
 
 
